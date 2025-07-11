bc907c62b45a01c3f12d61841644ad69dac72b82
MrDomoArigato98 <domdob98@gmail.com>
2025-07-11 09:35:38 +0100

    Complete project

---
+++ README.md
@@ -0,0 +1,3 @@
+## [The Odin Project - Mini Message Board ](https://www.theodinproject.com/lessons/node-path-nodejs-mini-message-board)
+
+Simple app to practice Node.js using Express, and EJS.
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("node:path");
+const { execPath } = require("node:process");
+const app = express();
+const assetsPath = path.join(__dirname, "public");
+const PORT = 3000;
+
+app.use(express.static(assetsPath));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+app.get("/message/:index", (req, res) => {
+  const { index } = req.params;
+  const message = messages[index];
+
+  res.render("messageDetails", { message: message });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  const message = req.body.message;
+  const name = req.body.name;
+  messages.push({ text: message, user: name, added: new Date() });
+
+  console.log(`Added User: ${name} `);
+  console.log(`Message: ${message} `);
+
+  res.redirect("/");
+});
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
