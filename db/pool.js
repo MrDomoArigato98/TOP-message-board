df0da4cc74d3819edfff84a8c5376aaf0a58a704
MrDomoArigato98 <domdob98@gmail.com>
2025-07-17 14:56:02 +0100

    fixes

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,9 @@ require("dotenv").config();
 
 // Again, this should be read from an environment variable
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: "require",
 });
